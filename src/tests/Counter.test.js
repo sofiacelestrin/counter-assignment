e168a85c46f709efc511c864242918f082283c5b
sofiacelestrin <101586639+sofiacelestrin@users.noreply.github.com>
2024-04-09 14:24:51 -0400

    Added counter test

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,36 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import Counter from '../components/Counter';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+// Get the variable count and make sure it is equal to 0
+// getByTestId
+  const initialCount = screen.getByTestId('count');
+  expect(initialCount).toHaveTextContent('0');
 });
 
 test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  // Need the count variable
+  const count = screen.getByTestId('count');
+  const add = screen.getByText('+');
+  userEvent.click(add);
+  expect(count).toHaveTextContent('1');
 });
 
 test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+  // Need the count variable
+  const count = screen.getByTestId('count');
+  const minus = screen.getByText('-');
+  userEvent.click(minus);
+  expect(count).toHaveTextContent('-1');
 });
+
